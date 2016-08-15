eebe3fb11eeaa994447c11768d1a58069ae9a64f
Rachel Lyons <rlyons21@hotmail.com>
2016-08-15 10:40:30 -0500

    spaces instead of underscores in drop down, can look at drop down for all input fields now

--- global.js
+++ global.js
@@ -17,6 +17,10 @@ window.addEventListener("load", function(){
     		if (theater_request.status === 200) {
 				var response = JSON.parse(theater_request.response);
 				var theater_arr = Object.keys(response.theaters);
+				for(i=0; i<theater_arr.length; i++){
+						theater_arr[i] = theater_arr[i].split("_").join(" ");
+					}
+
 
 				// Loop over the JSON array.
 				theater_arr.forEach(function(item) {
@@ -36,7 +40,7 @@ window.addEventListener("load", function(){
 
 
 
-	show.addEventListener("click", function(){
+	show.addEventListener("mouseenter", function(){
 		while(show_data.children.length > 0){
 			show_data.removeChild(show_data.children[0]);
 		}
@@ -44,11 +48,15 @@ window.addEventListener("load", function(){
 		show_request = new XMLHttpRequest();
 
 		show_request.onreadystatechange = function(response) {
-			var theater = document.getElementById("theater").value;
+			var theater = document.getElementById("theater").value.split(" ").join("_");
 	  		if (show_request.readyState === 4) {
 	    		if (show_request.status === 200) {
 					var response = JSON.parse(show_request.response);
 					var show_arr = Object.keys(response.theaters[theater]);
+
+					for(i=0; i<show_arr.length; i++){
+						show_arr[i] = show_arr[i].split("_").join(" ");
+					}
 
 					// Loop over the JSON array.
 					show_arr.forEach(function(item) {
@@ -69,7 +77,7 @@ window.addEventListener("load", function(){
 	});
 
 
-	nominee.addEventListener("click", function(){
+	nominee.addEventListener("mouseenter", function(){
 		while(nominee_data.children.length > 0){
 			nominee_data.removeChild(nominee_data.children[0]);
 		}
@@ -77,12 +85,16 @@ window.addEventListener("load", function(){
 		nominee_request = new XMLHttpRequest();
 
 		nominee_request.onreadystatechange = function(response) {
-			var show = document.getElementById("show").value;
-			var theater = document.getElementById("theater").value;
+			var show = document.getElementById("show").value.split(" ").join("_");
+			var theater = document.getElementById("theater").value.split(" ").join("_");
 	  		if (nominee_request.readyState === 4) {
 	    		if (nominee_request.status === 200) {
 					var response = JSON.parse(nominee_request.response);
 					var nominee_arr = Object.keys(response.theaters[theater][show]);
+
+					for(i=0; i<nominee_arr.length; i++){
+						nominee_arr[i] = nominee_arr[i].split("_").join(" ");
+					}
 
 					// Loop over the JSON array.
 					nominee_arr.forEach(function(item) {
@@ -105,7 +117,7 @@ window.addEventListener("load", function(){
 	
 
 
-	role.addEventListener("click", function(){
+	role.addEventListener("mouseenter", function(){
 		while(role_data.children.length > 0){
 			role_data.removeChild(role_data.children[0]);
 		}
@@ -113,14 +125,16 @@ window.addEventListener("load", function(){
 		role_request = new XMLHttpRequest();
 
 		role_request.onreadystatechange = function(response) {
-			var nominee = document.getElementById("nominee").value;
-			var show = document.getElementById("show").value;
-			var theater = document.getElementById("theater").value;
+			var nominee = document.getElementById("nominee").value.split(" ").join("_");
+			var show = document.getElementById("show").value.split(" ").join("_");
+			var theater = document.getElementById("theater").value.split(" ").join("_");
 	  		if (role_request.readyState === 4) {
 	    		if (role_request.status === 200) {
 					var response = JSON.parse(role_request.response);
-					debugger;
 					var role_arr = response.theaters[theater][show][nominee];
+					for(i=0; i<role_arr.length; i++){
+						role_arr[i] = role_arr[i].split("_").join(" ");
+					}
 
 					// Loop over the JSON array.
 					role_arr.forEach(function(item) {
